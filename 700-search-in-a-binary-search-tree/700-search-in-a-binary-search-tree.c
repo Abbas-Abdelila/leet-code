3b607d42219c1473747d390e1b3ea59c321adb27
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-02-15 05:16:37 +0300

    Time: 35 ms (43.95%), Space: 15.4 MB (30.97%) - LeetHub

---
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* searchBST(struct TreeNode* root, int val){
+    if(root==NULL) {
+        return root;
+    }
+    
+    if( root->val == val){
+        return root;
+    }
+    
+    if(root->val > val) {
+        root = searchBST(root->left, val);
+    }
+    else if (root->val < val )
+        root = searchBST(root->right, val);
+    
+    return root;
+
+    
+}
