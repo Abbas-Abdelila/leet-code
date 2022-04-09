71b702ec77b50278447243e6c78b77477631c87f
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-04-09 22:51:00 +0300

    Time: 12 ms (97.15%), Space: 13.6 MB (52.51%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -1,9 +1,7 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        words = s.strip().split(' ') #strip gets rid of whitespace on the left and right
-        return len(words[len(words)-1])
+        words = s.split()
+        
+        return len(words[-1])
+        
         
