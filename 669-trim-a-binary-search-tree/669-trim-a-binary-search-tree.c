aad8fb09db0dbc5c765eefb1302da146412e7114
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-04-15 15:19:08 +0300

    Time: 17 ms (40.91%), Space: 10.7 MB (36.36%) - LeetHub

---
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.c
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* trimBST(struct TreeNode* root, int low, int high){
+    if(root == NULL) {
+        return NULL;
+    }
+    
+    if(root->val > high) {
+        return trimBST(root->left, low, high);
+    }
+    else if (root->val < low) {
+        return trimBST(root->right, low, high);
+    }
+    
+    else {
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+    }
+    
+    return root;
+}
