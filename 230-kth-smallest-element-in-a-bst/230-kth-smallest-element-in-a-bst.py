02858e08df38877e82cbaa3249706b50f3f0ac15
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-04-18 13:39:42 +0300

    Time: 54 ms (83.85%), Space: 18.1 MB (48.12%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -7,17 +7,46 @@
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         
-        array = []
-        def traverse(node):
-            if node == None:
-                return
-            traverse(node.left)
-            nonlocal array
-            array.append(node.val)
-            traverse(node.right)
+#         array = []
+#         def traverse(node):
+#             if node == None:
+#                 return
+#             traverse(node.left)
+#             nonlocal array
+#             array.append(node.val)
+#             traverse(node.right)
         
-        traverse(root);
+#         traverse(root);
         
-        if array:
-            return array[k-1]
-        return -1
+#         if array:
+#             return array[k-1]
+#         return -1
+
+#         Time and Space Complexity -> O(n)
+
+#         Efficent solution
+                      
+        
+        left = k
+        result = None
+            
+        def inorder(node):
+            if node is None:
+                return 
+
+            if(k<0):
+                return -1
+            
+            inorder(node.left)
+
+            nonlocal left
+            left-=1
+            if left==0:
+                nonlocal result
+                result = node.val
+
+            inorder(node.right)
+        
+        inorder(root);
+        return result
+    # Time Complexity -> O(h)  Space Complexity -> O(1)
