9dc3688f5d3b00acd2639d40275db3b075208500
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-04-22 03:16:26 +0300

    Time: 346 ms (41.82%), Space: 40.5 MB (12.79%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,13 +1,13 @@
 class MyHashSet:
 
     def __init__(self):
-        self.arr = [0] * (10 ** 6 + 1)
+        self.arr = [False] * (10 ** 6 + 1)
 
     def add(self, key: int) -> None:
-        self.arr[key] = 1
+        self.arr[key] = True
 
     def remove(self, key: int) -> None:
-        self.arr[key] = 0
+        self.arr[key] = False
         
     def contains(self, key: int) -> bool:
         return self.arr[key]
