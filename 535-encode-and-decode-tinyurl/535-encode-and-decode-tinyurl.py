43f4b5ee63b4be74ce327398372e30220aa76750
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-04-23 17:01:46 +0300

    Time: 146 ms (5.19%), Space: 18 MB (6.61%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
@@ -1,17 +1,40 @@
-class Codec:
+import string
+import random
 
+class Codec:
+    
+    
+    dictionary = {}
+    characters = list(string.ascii_letters + string.digits)
+    
+    def pass_generator(self):
+        
+        random.shuffle(self.characters)
+        
+        password = []
+        for i in range(5):
+            password.append(random.choice(self.characters))
+        
+        return "".join(password)
+    
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        return longUrl
-        
+        shortened = self.pass_generator()
+        self.dictionary[longUrl] = "http://tinyurl.com/"+shortened
+        print(self.dictionary)
+        return self.dictionary[longUrl]
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        return shortUrl
-        
-
+        keys = [k for k,v in self.dictionary.items() if v==shortUrl]
+        if keys:
+            return keys[0]
+        return ""
+    
+ # A hacky approach is just return the input for both the decoder and encoder since you call the encode first and get the put it in decoder you will get your desired output withoit doing any thing
+ 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.decode(codec.encode(url))
