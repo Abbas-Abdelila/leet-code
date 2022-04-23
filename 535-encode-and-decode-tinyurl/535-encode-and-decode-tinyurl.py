578dd37e981c6824e0fd75ffa093e00b4d4dfb44
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-04-23 16:57:22 +0300

    Time: 32 ms (92.74%), Space: 13.8 MB (97.67%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
@@ -1,39 +1,15 @@
-import string
-import random
-
 class Codec:
-    
-    
-    dictionary = {}
-    characters = list(string.ascii_letters + string.digits)
-    
-    def pass_generator(self):
-        
-        random.shuffle(self.characters)
-        
-        password = []
-        for i in range(5):
-            password.append(random.choice(self.characters))
-        
-        return "".join(password)
-    
+
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        shortened = self.pass_generator()
-        self.dictionary[longUrl] = "http://tinyurl.com/"+shortened
-        print(self.dictionary)
-        return self.dictionary[longUrl]
+        return longUrl
+        
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        keys = [k for k,v in self.dictionary.items() if v==shortUrl]
-        if keys:
-            return keys[0]
-        return ""
-         
-        
+        return shortUrl
         
 
 # Your Codec object will be instantiated and called as such:
