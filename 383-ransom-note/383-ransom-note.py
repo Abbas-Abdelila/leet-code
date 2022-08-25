13a0cf5644008846e6be1e5f81dc95c10c088dba
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-08-25 11:05:48 +0300

    Time: 80 ms (66.33%), Space: 13.6 MB (59.17%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,11 +1,24 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-            magazine_dict = collections.Counter(magazine)    
-            for ch in ransomNote:
-                if ch in magazine and ransomNote.count(ch) <= magazine_dict[ch]:
-                    continue
-                else: 
-                    return False
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        chars = [0] * 26
+        for ch in magazine:
+            chars[ord(ch)-ord('a')] += 1
+        
+        for ch in ransomNote:
+            chars[ord(ch)-ord('a')] -= 1
             
-            return True
+            if chars[ord(ch)-ord('a')] == -1:
+                return False
+        
+        return True
             
+        
+        
+        
+        
+        
