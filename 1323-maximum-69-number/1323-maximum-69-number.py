2a7cb0aa74f1fb97e45f83823d8819f11c4556a4
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2022-11-07 18:17:59 +0300

    Time: 58 ms (41.91%), Space: 13.9 MB (54.69%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,10 +1,19 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        numbers = list(str(num))
-        for i, n in enumerate(numbers):
-            if n != '9':
-                numbers[i] = '9'
-                break
+        n = num
+        for i in reversed(range(4)): # range(4, 0, -1)
+            d, n = divmod(n, 10**i)
+            if d == 6:
+                return num + 3*10**i # adds three to the respective 10th place
+        return num     
         
-        return int("".join(numbers))
+        
+        # -> With string
+#         numbers = list(str(num))
+#         for i, n in enumerate(numbers):
+#             if n != '9':
+#                 numbers[i] = '9'
+#                 break
+        
+#         return int("".join(numbers))
         
