f974e6175034aea594bce5d3016b39384421f3dd
Abbas-Abdelila <66496326+Abbas-Abdelila@users.noreply.github.com>
2023-01-02 04:27:32 +0300

    Time: 38 ms (74.72%), Space: 13.8 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,5 +1,13 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        return word.isupper() or word.islower() or word.istitle()
-            
+        # Use the count of capitals
+        num_of_capital = 0
+        N = len(word)
         
+        for ch in word:
+            num_of_capital += ch.isupper()
+            
+        return ((num_of_capital == N) or 
+               (num_of_capital == 1 and word[0].isupper()) or 
+               (num_of_capital == 0))
+            
